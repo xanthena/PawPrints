389e5c7f639fb488fb105b1128d284b5296a4080
Riz Lala <rizlala123@gmail.com>
2026-07-04 03:11:30 +0530

    Adding prompt and qwen test file

---
+++ vision_evaluator/timestamp_extractor_from_file.py
@@ -0,0 +1,19 @@
+import os
+def timestamp_extractor_from_file(frame):
+    filename = os.path.basename(frame)
+    # resized_frame_000000_0.00s.jpg
+
+    # Remove extension
+    filename = os.path.splitext(filename)[0]
+    # resized_frame_000000_0.00s
+
+    # Split by "_"
+    parts = filename.split("_")
+
+    frame_number = int(parts[2])
+
+    timestamp = float(parts[3].replace("s", ""))
+
+    print(frame_number)
+    print(timestamp)
+    return frame_number,timestamp
